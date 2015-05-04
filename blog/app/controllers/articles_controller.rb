410aeefb630a614128c47d33ebb8ddfe4b27c510
setohe0909 <setohe.09@gmail.com>
2015-05-04 16:14:04 -0500

    changes

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -8,4 +8,19 @@ class ArticlesController < ApplicationController
 	def show
 		@article = Article.find(params[:id])
 	end
+
+	#GET /article/new
+	def new
+		@article = Article.new
+
+	end
+
+	#POST /article
+	def create
+		@article = Article.new(title: params[:article][:title],
+							   body: params[:article][:title])
+
+		@article.save
+		redirect_to @article
+	end
 end
