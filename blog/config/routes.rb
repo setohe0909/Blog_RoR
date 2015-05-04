b810c6aa4d5adaac720b095bd89fca9d8c177543
seto <zero@seto-zero.(none)>
2015-05-04 12:38:23 -0500

    creacion de modelos controladores

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
-  # get 'welcome/index'
+  
+  resources :articles
 
+=begin
+    get "/articles" index
+    get "/articles/:id"  show
+    get "/articles/new" new
+    get "/articles/:id/edit" edit
+    post "/articles" create
+    delete "/articles" delete
+    patch "/articles/:id" update
+    put "/articles/:id" update
+=end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
